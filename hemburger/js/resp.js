4f00dd9294ce895c6473629200bf243003e92dc3
afroz <141931289+Afrozi@users.noreply.github.com>
2024-04-24 13:34:38 +0530

    Add files via upload

---
+++ hemburger/js/resp.js
@@ -0,0 +1,12 @@
+let burger = document.querySelector(".burger");
+let navbar = document.querySelector(".navbar");
+let navLlist = document.querySelector(".nav-list");
+let rightNav = document.querySelector(".rightnav");
+
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle("v-class");
+    navLlist.classList.toggle("v-class");
+    navbar.classList.toggle("h-nav");
+
+})
